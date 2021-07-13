8d0d59dc1e8a11ce633601976c6affbe4fbfb33f
saiprasaths <saiprasathpgm2003@gmail.com>
2021-07-13 18:12:55 +0530

    second commit

---
+++ planning.js
@@ -0,0 +1,26 @@
+//!  Book my show planning
+
+//*-> We need a reusable carousal
+//  ?-> Hero carousel
+//  ?-> Common carousel
+//  ?-> Cast and crew small carousel
+
+
+// * Pages
+// -> Home page
+// -> Movie page
+// -> Categories page
+// -> Auth page
+// -> Razorpay payment gateway
+
+// *Plan-> How are we building it????🤔
+// Layouts
+// Routes
+// Pages
+// Components
+
+// *HOC
+// Higher Order Components
+
+
+// ! Layout First Approach
